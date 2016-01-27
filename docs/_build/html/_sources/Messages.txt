2b7b7ec0aaa942517e098674945947a11a9e406e
rvanderh-hhnk <rvanderh-hhnk@snapunit.com>
2016-01-27 18:11:51 +0100

    test

---
+++ docs/_build/html/_sources/Messages.txt
@@ -0,0 +1,12 @@
+########
+Messages
+########
+De belangrijkste messages: "*upload succesvol*" of "*upload niet gelukt, pas gef aan*" worden geregeld bij het opslaan van waypoint in functie "save_gef" in **TestGefData.py**.
+
+Ook in functie "upload_gef" in views.py worden enkele foutmeldingen gegenereerd als daar een fout optreed. Bijvoorbeeld als er iets misgaat bij het maken van de dictionary d_GEF in  **UtlGefOpen.py** wordt hier een foutmelding gegeven: "*fout bij uitlezen gef, pas gef aan*"
+
+**Home.html**
+
+De weergave wordt geregeld in het {% block messages %} in home.html.
+
+Messages hebben een tag met "*info*" of "*error*" en worden respectievelijk met groen of rood weergegeven.  Alle messages worden in een lijst opgeslagen in django per request en op het einde weergegeven.
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # GUP-docs documentation build configuration file, created by
-# sphinx-quickstart on Tue Jan 26 11:47:20 2016.
+# sphinx-quickstart on Wed Jan 27 18:07:11 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -108,8 +107,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
